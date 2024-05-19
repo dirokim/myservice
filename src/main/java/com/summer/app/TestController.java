a6d9989237992e91c23c980cb094332636cc5ea4
dirokim <dirokim7@gmail.com>
2024-05-19 23:49:53 +0900

    fix body

--- src/main/java/com/summer/app/TestController.java
+++ src/main/java/com/summer/app/TestController.java
@@ -1,6 +1,7 @@
 package com.summer.app;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TestController {
 
 	@GetMapping("/")
-	public String test() throws Exception{
+	public String test(Model model) throws Exception{
+		model.addAttribute("path","temp/body");
 		return "index";
 	}
 }
